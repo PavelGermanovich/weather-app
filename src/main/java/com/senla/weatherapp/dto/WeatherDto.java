0748385058a829d4aff40b4a6b0e0f6b0b93907f
Germanovich Pavel <germanovichpavel95@gmail.com>
2023-03-24 16:24:24 +0300

    feat - small changes + java 11 and spring boot version change

--- src/main/java/com/senla/weatherapp/dto/WeatherDto.java
+++ src/main/java/com/senla/weatherapp/dto/WeatherDto.java
@@ -24,13 +24,14 @@ public class WeatherDto {
     private void unpackNestedLocation(Map<String, String> location) {
         this.location = location.get("name");
     }
-
+    
     @JsonProperty("current")
     private void unpackNestedCurrentWeather(Map<String, Object> currentWeather) {
         this.weatherCondition = ((Map<String, String>) currentWeather.get("condition")).get("text");
         this.temperature = (Double) currentWeather.get("temp_c");
         this.humidity = (Integer) currentWeather.get("humidity");
         this.pressureMb = (Double) currentWeather.get("pressure_mb");
+        //We are getting wind speed in KM per hour need to be converted in Meters per hour due to the task spec
         this.windSpeedMtrHr = ((Double) currentWeather.get("wind_kph")) * 1000;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         this.weatherDate = LocalDateTime.parse((String) currentWeather.get("last_updated"), formatter).toLocalDate();
